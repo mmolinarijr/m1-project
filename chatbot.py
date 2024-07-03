6f8a9145fcc41360d22503e3fbc4229cb94bf769
Mauricio Molinari <mmolinarijr@gmail.com>
2024-07-03 18:14:42 -0300

    Added Flask

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,40 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-from train_chatbot import training_data
+from train_chatbot import training_data1, training_data2
 import colorama
 from colorama import Fore, Style
+from flask import Flask, render_template, request, jsonify
 
 colorama.init(autoreset=True)
+
+# Create and define Flask routes
+app = Flask(__name__)
+
+@app.route('/')
+def intro():
+    return render_template('chat.html', options=options)
+
+@app.route('/ask', methods = ['POST'])
+def ask():
+    message = request.form['messageText']
+
+    bot_response = chatbot.get_response(message)
+
+    while True:
+        try: 
+            bot_response = chatbot.get_response(message)
+
+            minimum_level_of_reliability = 0.4
+
+            if float(bot_response.confidence) > minimum_level_of_reliability:
+                bot_response = str(bot_response)
+                return jsonify({'status':'OK','answer':bot_response})
+            else:
+                bot_response = 'Desculpe, não entendi. Tente novamente ou entre em contato conosco via WhatsApp (41) 9 92641020.'
+                return jsonify({'status': 'OK', 'answer': bot_response})
+        except IndexError:
+            bot_response = 'Ocorreu um erro ao processar sua solicitação.'
+            return jsonify({'status':'ERROR','answer':bot_response})
 
 chatbot_name = 'M1 Motors'
 
@@ -12,38 +42,43 @@ chatbot_name = 'M1 Motors'
 chatbot = ChatBot(
     chatbot_name,
     read_only=True,
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace'
+    ],
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=['chatterbot.logic.BestMatch'],
+    logic_adapters=['chatterbot.logic.BestMatch', 'chatterbot.logic.SpecificResponseAdapter'],
     database_uri='sqlite:///db.sqlite3'
 )
 
-# Define the training file
+# Define the training files
 trainer = ListTrainer(chatbot)
-trainer.train(training_data)
+trainer.train(training_data1)
+trainer.train(training_data2)
+
+# Define the initial options for user to choose from
+options = [
+    '1 - Qual o telefone de contato?',
+    '2 - Qual o e-mail de contato?',
+    '3 - Como eu acesso a politica de privacidade da empresa?',
+    '4 - Qual o endereço?',
+    '5 - Quem é a M1 Motors?',
+    '6 - Qual o site da empresa?',
+]
 
 # Function to display chatbot options
 def bot_options():
-    options = [
-        '1 - Qual o telefone de contato?',
-        '2 - Qual o e-mail de contato?',
-        '3 - Como eu acesso a politica de privacidade da empresa?',
-        '4 - Qual o endereço?',
-        '5 - Quem é a M1 Motors?',
-        '6 - Qual o site da empresa?',
-    ]
-
-    print('Escolha uma das opções a seguir:')
+    print('Escolha uma das opções a seguir:\n')
 
     for option in options:
         print(option)
 
-    print('\n' + 'Ou digite sua própria pergunta (digite sair para encerrar ou 0 para voltar ao menu):')
+    print('\n' + 'Ou digite sua própria pergunta (a qualquer momento digite sair para encerrar ou 0 para voltar ao menu):')
 
     return ''
 
-# Function to interact with bot
+# Function to interact with bot via terminal
 def chat_with_bot():
-    print('Olá, tudo bem? Como posso te ajudar?' + '\n')
+    print(f'Olá, tudo bem? Seja bem-vindo(a) a {chatbot_name}' + '\n')
 
     while True:
         try:
@@ -60,11 +95,19 @@ def chat_with_bot():
 
                     bot_response = chatbot.get_response(user_input)
 
-                    print(Fore.RED + chatbot_name + ':' + Style.RESET_ALL + f' {bot_response}')
+                    minimum_level_of_reliability = 0.4
+
+                    if float(bot_response.confidence) > minimum_level_of_reliability:
+                        print(Fore.RED + chatbot_name + ':' + Style.RESET_ALL + f' {bot_response}')
+                    else:
+                        print(Fore.RED + chatbot_name + ':' + Style.RESET_ALL + ': Desculpa não entendi, tente novamente, ')
+                        print(bot_options())
                 except (KeyboardInterrupt, EOFError, SystemExit):
                     return
         except (KeyboardInterrupt, EOFError, SystemExit):
             break
 
+# Start the app either terminal or web
 if __name__ == '__main__':
     chat_with_bot()
+    app.run(ssl_context='adhoc')
--- train_chatbot.py
+++ train_chatbot.py
@@ -1,38 +1,124 @@
+# Common responses about the company
+phone_number = 'O telefone de contato é (41) 3014-9646 e WhatsApp é (41) 9 9264-1020'
+email = 'O e-mail de contato é contato@m1motorscwb.com.br'
+privacy_policy = 'Acesse através deste link: https://m1motors.com.br/politica-de-privacidade'
+company_address = 'O endereço de todas as lojas está disponível neste link: https://m1motors.com.br/localizacao'
+who_is = 'Fundada em 2020 por um visionário com ampla experiência no mercado automotivo, a M1 Motors é uma empresa do ramo automotivo que se destaca pela inovação. Sua localização estratégica em Shopping Centers proporciona aos clientes maior conforto e segurança durante todo o processo de compra e venda de veículos.'
+company_website = 'Nosso site é: https://m1motors.com.br/'
+more_information = 'Para mais informações entre em contato através do e-mail: contato@m1motorscwb.com.br ou WhatsApp (41) 9 9264-1020. Também estamos nas redes sociais como @m1motorsoficial'
+help_how = 'Como posso te ajudar? Digite 0 para listar as opções ou sair para encerrar o chat'
+unknown_option = 'Opção não reconhecida, favor tente novamente'
+
 # Trained responses in PT-BR
-training_data = [
+training_data1 = [
     'Qual o telefone de contato?',
-    'O telefone de contato é (41) 3014-9646 e WhatsApp é (41) 9 9264-1020',
+    phone_number,
     'Qual o e-mail de contato?',
-    'O e-mail de contato é contato@m1motorscwb.com.br',
+    email,
     'Como eu acesso a politica de privacidade da empresa?',
-    'Acesse através deste link: https://m1motors.com.br/politica-de-privacidade',
+    privacy_policy,
     'Qual o endereço?',
-    'O endereço de todas as lojas está disponível neste link: https://m1motors.com.br/localizacao',
+    company_address,
+    'qual o endereço da loja',
+    company_address,
+    'loja',
+    company_address,
     'Quem é a M1 Motors?',
-    'Fundada em 2020 por um visionário com ampla experiência no mercado automotivo, a M1 Motors é uma empresa do ramo automotivo que se destaca pela inovação. Sua localização estratégica em Shopping Centers proporciona aos clientes maior conforto e segurança durante todo o processo de compra e venda de veículos.',
+    who_is,
     'Qual o site da empresa?',
-    'Nosso site é: https://m1motors.com.br/'
+    company_website,
     'Preciso de mais informações',
-    'Para mais informações entre em contato através do e-mail: contato@m1motorscwb.com.br'
-]
-
-# Alternative questions
-training_data = [
-    '1',
-    'O telefone de contato é (41) 3014-9646 e WhatsApp é (41) 9 9264-1020',
-    '2',
-    'O e-mail de contato é contato@m1motorscwb.com.br',
-    '3',
-    'Acesse através deste link: https://m1motors.com.br/politica-de-privacidade',
-    '4',
-    'O endereço de todas as lojas está disponível neste link: https://m1motors.com.br/localizacao',
-    '5',
-    'Fundada em 2020 por um visionário com ampla experiência no mercado automotivo, a M1 Motors é uma empresa do ramo automotivo que se destaca pela inovação. Sua localização estratégica em Shopping Centers proporciona aos clientes maior conforto e segurança durante todo o processo de compra e venda de veículos.',
-    '6',
-    'Nosso site é: https://m1motors.com.br/',
+    more_information,
+    'oi',
+    help_how,
+    'ola',
+    help_how,
+    'olá',
+    help_how,
+    'tudo bem?',
+    help_how,
+    'qual a forma de pagamento',
+    more_information,
+    'pagamento',
+    more_information,
+    'ajuda',
+    more_information,
+    'modelo de carro',
+    more_information,
+    'Quais marcas de veículos a M1 Motors oferece?',
+    'A M1 Motors atualmente conta com 32 marcas diferentes em seu estoque, incluindo Volkswagen, Chevrolet, Renault, Fiat, Ford, Hyundai, Jeep, Honda, Toyota, Peugeot, Nissan e BMW',
+    'Como posso entrar em contato com a M1 Motors?',
+    'Você pode entrar em contato via WhatsApp (41) 9 9264-1020 ou pelo telefone (41) 3014-9646. Além disso, também estamos presentes no Instagram, Facebook e TikTok como @m1motorsoficial',
+    'Qual é a especialidade da M1 Motors?',
+    'A M1 Motors é conhecida por oferecer carros seminovos, usados e zero em Curitiba. Temos uma variedade de modelos de multimídias automotivas',
+    'Qual é o horário de funcionamento da M1 Motors?',
+    'A M1 Motors está aberta de segunda a sexta-feira, das 8h às 18h, e aos sábados, das 9h às 13h.',
+    'Quais são os documentos necessários para comprar um veículo na M1 Motors?',
+    'Para a compra de um veículo, você precisará apresentar RG, CPF, comprovante de residência e comprovante de renda.',
+    'A M1 Motors oferece serviços de consórcio?',
+    'Sim, temos opções de consórcio para quem deseja adquirir um veículo de forma parcelada.',
+    'Quais são as opções de pagamento aceitas pela M1 Motors?',
+    'Aceitamos pagamento à vista, financiamento bancário e cartões de crédito para aquisição de veículos.',
+    'Quais são os benefícios de comprar um veículo seminovo na M1 Motors?',
+    'Os veículos seminovos geralmente têm preços mais acessíveis e ainda contam com garantia e revisão prévia.',
+    'quem é você?',
+    who_is,
+    'quem são vocês?',
+    who_is,
+    'endereço',
+    company_address,
+    'telefone',
+    phone_number,
     'preciso de ajuda',
-    'Para mais informações entre em contato através do e-mail: contato@m1motorscwb.com.br'
+    more_information,
+    'carro',
+    more_information,
+    'quero comprar um carro',
+    more_information,
+    'comprar',
+    more_information,
+    'compra de um veiculo',
+    more_information,
+    'vender',
+    more_information,
+    'quero vender um carro',
+    more_information,
+    'vender carro',
+    more_information,
+    'Vocês têm carros elétricos ou híbridos?',
+    more_information,
+    'Como posso entrar em contato com um vendedor?',
+    more_information,
+    'Vocês têm promoções especiais ou descontos atualmente?',
+    more_information,
+    'Vocês aceitam troca de veículos?',
+    more_information,
+    'Como posso agendar um test drive?',
+    more_information,
+    'preciso de ajuda pra comprar um carro',
+    more_information
 ]
 
-# Predefined responses
-corpus = "chatterbot.corpus.Portuguese"
+# Alternative questions based on the menu list
+training_data2 = [
+    '1',
+    phone_number,
+    '2',
+    email,
+    '3',
+    privacy_policy,
+    '4',
+    company_address,
+    '5',
+    who_is,
+    '6',
+    company_website,
+    '7',
+    unknown_option,
+    '8',
+    unknown_option,
+    '9',
+    unknown_option,
+    '10',
+    unknown_option
+]
